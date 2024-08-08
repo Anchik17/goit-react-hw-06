319d2de34a4e001edae7ac9af59d0aac7f80f440
Anchik17 <151466304+Anchik17@users.noreply.github.com>
2024-08-08 19:57:56 +0200

    Phonebook done

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,31 @@
+import { FaUser } from 'react-icons/fa6';
+import { BsFillTelephoneFill } from 'react-icons/bs';
+import s from './Contact.module.css';
+import { useDispatch } from 'react-redux';
+import { deleteContacts } from '../../redux/contactsSlice';
+
+const Contact = ({ name, number, id }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className={s.container}>
+      <div>
+        <p>
+          <FaUser /> {name}
+        </p>
+        <p>
+          <BsFillTelephoneFill /> {number}
+        </p>
+      </div>
+      <button
+        className={s.btn}
+        type='button'
+        onClick={() => dispatch(deleteContacts(id))}
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,29 @@
+import { useSelector } from 'react-redux';
+import Contact from '../Contact/Contact';
+import s from './ContactList.module.css';
+import { selectContacts, selectNameFilter } from '../../redux/selectors';
+
+const ContactList = () => {
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter);
+
+  const filteredData = contacts.filter((item) =>
+    item.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <ul className={s.list}>
+      {filteredData.map((contact) => (
+        <li className={s.item} key={contact.id}>
+          <Contact
+            name={contact.name}
+            number={contact.number}
+            id={contact.id}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ContactList;
